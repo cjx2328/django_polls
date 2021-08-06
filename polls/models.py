0c2bb067a2fcde5ef4143f701e83f6e05df90e56
kurtchen <cjx2328@126.com>
2021-08-06 17:11:59 +0800

    add data edit ui

--- polls/models.py
+++ polls/models.py
@@ -7,13 +7,13 @@ class Question(models.Model):
     # ...
     def was_published_recent(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-    # question_text = models.CharField(max_length=200)
-    # pub_date = models.DateTimeField('date published')
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
 
 class Choice(models.Model):
-    # question = models.ForeignKey(Question,on_delete=models.CASCADE)
-    # choice_text = models.CharField(max_length=200)
-    # votes = models.IntegerField(default=0)
+    question = models.ForeignKey(Question,on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
     # ...
     def __str__(self):
         return self.choice_text
